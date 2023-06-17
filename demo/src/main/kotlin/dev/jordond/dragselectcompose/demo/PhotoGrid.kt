92df1dd6d2340ed88dad86d83cd5ed92a053db8a
Jordon de Hoog <jordon.dehoog@gmail.com>
2023-06-16 21:29:25 -0400

    add Modifier.animateSelection

--- demo/src/main/kotlin/dev/jordond/dragselectcompose/demo/PhotoGrid.kt
+++ demo/src/main/kotlin/dev/jordond/dragselectcompose/demo/PhotoGrid.kt
@@ -1,20 +1,13 @@
 package dev.jordond.dragselectcompose.demo
 
-import androidx.compose.animation.core.animateDp
-import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
@@ -32,6 +25,7 @@ fun PhotoGrid(
     dragSelectState: DragSelectState<PhotoItem> = rememberDragSelectState(),
 ) {
     LazyDragSelectVerticalGrid(
+        modifier = modifier,
         columns = GridCells.Adaptive(minSize = 128.dp),
         items = photoItems,
         state = dragSelectState,
@@ -41,44 +35,21 @@ fun PhotoGrid(
         items(key = { it.id }) { photo ->
             SelectableItem(
                 item = photo,
+                animateSelection = false,
                 selectedIcon = { SelectedIcon(Modifier.align(Alignment.BottomEnd)) },
                 unselectedIcon = { UnselectedIcon(Modifier.align(Alignment.BottomEnd)) },
-            ) { selected ->
-                ImageItem(
-                    photo = photo,
-                    selected = selected,
-                )
+            ) {
+                Surface(
+                    modifier = modifier.aspectRatio(1f),
+                    tonalElevation = 3.dp
+                ) {
+                    Image(
+                        painter = rememberAsyncImagePainter(photo.url),
+                        contentDescription = null,
+                        modifier = Modifier
+                    )
+                }
             }
-        }
-    }
-}
-
-@Composable
-private fun ImageItem(
-    photo: PhotoItem,
-    selected: Boolean,
-    modifier: Modifier = Modifier,
-) {
-    Surface(
-        modifier = modifier.aspectRatio(1f),
-        tonalElevation = 3.dp
-    ) {
-        Box {
-            val transition = updateTransition(selected, label = "selected")
-            val padding by transition.animateDp(label = "padding") { selected ->
-                if (selected) 10.dp else 0.dp
-            }
-            val roundedCornerShape by transition.animateDp(label = "corner") { selected ->
-                if (selected) 16.dp else 0.dp
-            }
-            Image(
-                painter = rememberAsyncImagePainter(photo.url),
-                contentDescription = null,
-                modifier = Modifier
-                    .matchParentSize()
-                    .padding(padding)
-                    .clip(RoundedCornerShape(roundedCornerShape))
-            )
         }
     }
 }
