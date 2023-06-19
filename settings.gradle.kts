3cc8fb126d44c4539608d2c76600a55b690f6347
Jordon de Hoog <jordon.dehoog@gmail.com>
2023-06-18 20:53:45 -0400

    update plugin repos

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,9 @@
 pluginManagement {
     repositories {
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
         mavenCentral()
-        gradlePluginPortal()
     }
 }
 
@@ -10,6 +11,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
