f29ebc897f16dc44104837a2c46e95eb179423a9
Jordon de Hoog <jordon.dehoog@gmail.com>
2023-06-18 20:03:26 -0400

    add gradle build scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,10 +5,25 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+    }
+}
+
+plugins {
+    id("com.gradle.enterprise") version "3.9"
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
     }
 }
 
