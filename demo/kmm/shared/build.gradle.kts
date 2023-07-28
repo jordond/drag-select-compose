5a57a59d5e9e721f1ebd1b97521e090050860ae5
Jordon de Hoog <jordon.dehoog@gmail.com>
2023-07-28 11:59:09 -0400

    set demo to use java 17

--- demo/kmm/shared/build.gradle.kts
+++ demo/kmm/shared/build.gradle.kts
@@ -85,11 +85,11 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlin {
-        jvmToolchain(jdkVersion = 11)
+        jvmToolchain(jdkVersion = 17)
     }
 }
